a07af3d184ffa3094ee81fe1b893f545694c4908
Jackson Delahunt <jackson@stemn.com>
2017-04-02 15:44:33 +1000

    handle all environments

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const hostname = require('os').hostname();
+const actionHostname = (forcedHostname) => {
 
-const actionHostname = store => next => action => {
-    const meta = Object.assign({}, action.meta, { hostname });
-    const actionWithMeta = Object.assign({}, action, { meta });
-    return next(actionWithMeta);
+    const randomHostname = () => `browser-${Math.random().toString(36).substr(2, 6)}`;
+
+    const getHostname = () => typeof window === 'undefined' || (typeof process !== 'undefined' && process.versions.electron)
+        ? require('os').hostname() // node.js, electron main thread, electron renderer
+        : randomHostname(); // browser
+
+    const hostname = forcedHostname || getHostname();
+
+    // middleware to assign hostname to meta
+    return store => next => action => {
+        const meta = Object.assign({}, action.meta, { hostname });
+        const actionWithMeta = Object.assign({}, action, { meta });
+        return next(actionWithMeta);
+    }
 }
 
 module.exports = actionHostname;
